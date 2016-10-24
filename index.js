48647bbc2215e198886925fb3be39af8ab4a7bc3
wungsen <fantasy.ws@gmail.com>
2016-10-24 18:47:02 +0800

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ wechat(conf.wechat);
 
 //创建自定义菜单，lib/menu.json 为菜单结构描述文件
 //相关文档： https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141013&token=&lang=zh_CN
-//wechat.createMenu(require('./lib/menu.json'));
+wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
